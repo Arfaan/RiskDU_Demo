d30759268270e4c9457ee2cb34c691f5e27626b5
Arfaan <amajothi@hotmail.com>
2015-05-17 10:25:05 +0100

    DU
    Demo

---
+++ RiskDU_Demo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RiskDU_Demo.Startup))]
+namespace RiskDU_Demo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
